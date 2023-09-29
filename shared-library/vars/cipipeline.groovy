16269d74acca51c65cf6494dd2484c89b522c73c
abdul <abdulsk054@gmail.com>
2023-09-29 12:01:05 +0530

    changed

--- shared-library/vars/cipipeline.groovy
+++ shared-library/vars/cipipeline.groovy
@@ -1,60 +1,26 @@
 def call() {
-  pipeline {
-      agent any
 
-      stages {
-          stage ('Compile Code') {
-            steps {
-             sh 'env'
-            }
-          }
-
-          stage ('Test') {
-            when {
-              allOf {
-                expression { env.BRANCH_NAME != null }
-                expression { env.TAG_NAME == null }
-              }
-
-            }
-            steps {
-              echo 'Hello World'
-            }
-          }
-
-          stage ('Code Quality') {
-            when {
-              allOf {
-                expression { env.BRANCH_NAME != null }
-                expression { env.TAG_NAME == null }
-              }
-
-            }
-            steps {
-              echo 'Hello World'
-            }
-          }
-
-          stage ('Code Security') {
-            when {
-              expression { BRANCH_NAME == "main" }
-            }
-              steps {
-                echo 'Hello World'
-              }
-          }
-
-          stage ('Release') {
-            when {
-              expression { env.TAG_NAME ==~ ".*" }
-            }
-            steps {
-              sh 'env'
-              echo 'Hello World'
-            }
-          }
+    node('workstation') {
+      stage ('Compile Code') {
+        common.compile()
       }
-  }
+
+      stage ('Test') {
+        print 'Hello'
+      }
+
+      stage ('Code Quality') {
+        print 'Hello'
+      }
+
+      stage ('Code Security') {
+        print 'Hello'
+      }
+
+      stage ('Release') {
+        print 'Hello'
+      }
+    }
 }
 
 
