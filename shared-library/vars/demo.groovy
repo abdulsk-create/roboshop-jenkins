538a0f9165654c5f8349004aaf0db4d3be29f8cd
abdul <abdulsk054@gmail.com>
2023-09-29 09:09:49 +0530

    terraform

--- shared-library/vars/demo.groovy
+++ shared-library/vars/demo.groovy
@@ -5,3 +5,39 @@ def info(message) {
 def warning(message) {
     echo "WARNING: ${message}"
 }
+
+def call() {
+  pipeline {
+      agent any
+
+      stages {
+          stage ('Compile') {
+            steps {
+              echo 'Hello World'
+              script {
+                info 'Starting'
+                warning 'Nothing to do!'
+              }
+            }
+          }
+
+          stage ('Test') {
+            steps {
+              echo 'Hello World'
+            }
+          }
+
+          stage ('Code Quality') {
+            steps {
+              echo 'Hello World'
+            }
+          }
+
+          stage ('App Deply') {
+            steps {
+              echo 'Hello World'
+            }
+          }
+      }
+  }
+}
