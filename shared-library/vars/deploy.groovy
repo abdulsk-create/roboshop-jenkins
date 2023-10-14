5c2cc1d5b6ceb1fd676c6a7a2d93c1ccc81367d6
abdul <abdulsk054@gmail.com>
2023-10-14 11:37:14 +0530

    Prod env

---
+++ shared-library/vars/deploy.groovy
@@ -0,0 +1,20 @@
+def call(){
+  pipeline {
+    agent any
+
+    parameters {
+      string(name: 'COMPONENT', defaultValue: '', description: 'Which Component to Deploy?')
+      string(name: 'ENV', defaultValue: '', description: 'Which Environment to Deploy?')
+      string(name: 'VERSION', defaultValue: '', description: 'Which Version to Deploy?')
+    }
+
+    stages {
+      stage ('Deploy') {
+        steps {
+          echo 'OKAY'
+        }
+      }
+    }
+  }
+}
+
